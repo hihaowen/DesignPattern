061e7ac34fa7ff7fedb61c5c197122066774d103
haowen.hi <haowen.hi@gmail.com>
2019-01-14 12:17:19 +0800

    抽象工厂

--- AbstractFactory.php
+++ AbstractFactory.php
@@ -1,45 +1,70 @@
 <?php
 
 /**
- * 抽象工厂
- *
- * 在简单工厂的基础上增加了抽象的概念
+ * 模式名称: 抽象工厂 (AbstractFactory)
+ * 模式类型: 创建型
+ * 模式描述: 对工厂抽象、对实现对象抽象，完全的多态设计
+ * 解决的问题: 将功能聚集，实现功能、调用分离，职责分工
+ * 优点: 在遵循原则的基础上互不影响
+ * 缺点: 写的类会比较多
+ * 和它类似的模式: 暂无
  */
 
-interface sendInterface
+// 发送接口
+interface SenderInterface
 {
-    public function send();
+    public function send($userId, $content);
 }
 
-class SmsSender implements sendInterface
+// 短信发送
+class SmsSender implements SenderInterface
 {
 
-    public function send()
+    public function send($userId, $content)
     {
         // TODO: Implement send() method.
     }
 
 }
 
-class EmailSender implements sendInterface
+// 邮件发送
+class EmailSender implements SenderInterface
 {
 
-    public function send()
+    public function send($userId, $content)
     {
         // TODO: Implement send() method.
     }
 
 }
 
-class SenderFactory
+// 发送工厂
+interface SenderFactory
 {
-    public static function getSmsSender()
+    /**
+     * @return SenderInterface
+     */
+    public function createSender() : SenderInterface;
+}
+
+// 短信
+class SmsFactory implements SenderFactory
+{
+
+    public function createSender() : SenderInterface
     {
         return new SmsSender();
     }
 
-    public static function getEmailSender()
+}
+
+// 邮件
+class EmailFactory implements SenderFactory
+{
+
+    public function createSender() : SenderInterface
     {
         return new EmailSender();
     }
+
 }
